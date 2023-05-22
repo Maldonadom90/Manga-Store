cb102df0c3133dbe408d8031581e9d507aa1c457
Mauricio Maldonado <maldonadom90@hotmail.com>
2023-05-22 20:37:19 -0300

    agregando partes del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-import './App.css'
+import "./App.css";
+import { Footer } from "./Footer";
+import { Home } from "./Home";
+import { Navbar } from "./Navbar";
 
 function App() {
-
-
   return (
-    
-  )
+    <>
+      <h1>Titulo</h1>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
